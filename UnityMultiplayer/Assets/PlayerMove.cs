20e6b7ea2e0f46a8a429530b8e8610928d983b5b
José Henrique Soares Silva <henrique.developer.net@gmail.com>
2017-08-30 01:55:58 -0300

    Non-Player Objects

--- UnityMultiplayer/Assets/PlayerMove.cs
+++ UnityMultiplayer/Assets/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 
 public class PlayerMove : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
 
     // Use this for initialization
     void Start()
@@ -19,9 +20,60 @@ public class PlayerMove : NetworkBehaviour
             return;
 
         var x = Input.GetAxis("Horizontal") * 0.1f;
-        var y = Input.GetAxis("Vertical") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
 
-        transform.Translate(x, 0, y);
+        transform.Translate(x, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Command function is called from the client, but invoked on the server
+            CmdFire();
+        }
 
     }
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        //This [Command] code is run on the server!
+
+        //Create the bullet object locally
+        //Create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity
+            );
+
+        //make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        //spaw the bullet on the clients
+        NetworkServer.Spawn(bullet);
+
+        //When the bullet is destroyt on the server
+        Destroy(bullet, 2.0f);
+
+    }
+
+    //void Fire()
+    //{
+    //    //Create the bullet object from the bullet prefab
+    //    var bullet = (GameObject)Instantiate(
+    //        bulletPrefab,
+    //        transform.position - transform.forward,
+    //        Quaternion.identity
+    //        );
+    //
+    //    //make the bullet move away in front of the player
+    //    bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+    //
+    //    //make bullet disappear after 2 seconds
+    //    Destroy(bullet, 2.0f);
+    //}
 }
